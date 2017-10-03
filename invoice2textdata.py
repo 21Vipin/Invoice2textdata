175fce2d00c5c7a7940813df36bb397416aa9c60
Vipin Makde <vipinmakde21@gmail.com>
2017-10-03 19:07:18 +0530

    Update invoice2textdata.py

--- invoice2textdata.py
+++ invoice2textdata.py
@@ -2,17 +2,24 @@ import json
 import os
 import sys
 import functions
+import zipfile
 
 def main():
-	path=sys.argv[1]
-	with open(path) as f:
-		config=json.load(f)
+    path=sys.argv[1]
+    with open(path) as f:
+        config=json.load(f)
+    
+    # unzip the zip file
+    zip_ref = zipfile.ZipFile(config['zip'], 'r')
+    zip_ref.extractall(config['src'])
+    zip_ref.close()
+    
 	
-	if os.path.exists(config['src']):
-		functions.convert(config['src'], config['des'])
+    if os.path.exists(config['src']):
+        functions.convert(config['src'], config['des'])
 
 if __name__ == '__main__':
-	if len(sys.argv)==1:
-		print("Please include the config.json file path like this - python invoice2textdata.py config.json")
-	else:
-		main()
+    if len(sys.argv)==1:
+        print("Please include the config.json file path like this - python invoice2textdata.py config.json")
+    else:
+        main()
